b373e71f2bb64d9eded466487932f52f21d7800e
The-Kharsair-Empire <cjy295042633@gmail.com>
2021-02-24 16:51:03 +1100

    update send-recv loop to receive tcp, work out the offset

--- Unity/Assets/Scripts/UnityClient.cs
+++ Unity/Assets/Scripts/UnityClient.cs
@@ -17,18 +17,23 @@ public class UnityClient : MonoBehaviour
     private NetworkStream stream;
 
     private StreamReader inChannel;
+
     private StreamWriter outChannel;
 
     //private GameObject End_effector_virtual_plane;
 
     private List<Action> trajectoryQueue;
+    private List<float[]> posQueue;
     private Dictionary<int, char> link_index_to_axis_mapper;
+    private Dictionary<int, float> link_offset;
+    private Dictionary<int, int> link_direction;
 
     public string host_ip = "localhost";
     public int host_port = 27;
 
     public GameObject VRController;
     public GameObject Virtual_end_effector;
+    public GameObject RobotActualTCPIndicator;
     public GameObject Movement_indicator; //assigned a cube of different color to Virtual_end_effector
 
 
@@ -49,12 +54,15 @@ public class UnityClient : MonoBehaviour
     public Vector3 previous_tcp_pos;
     public Quaternion previous_ee_orientation;
 
-    
+    private float[] cur_tcp_pos = null;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         trajectoryQueue = new List<Action>();
+        posQueue = new List<float[]>();
 
         link_index_to_axis_mapper = new Dictionary<int, char>() {
             {0, 'y' },
@@ -63,6 +71,28 @@ public class UnityClient : MonoBehaviour
             {3, 'x' },
             {4, 'y' },
             {5, 'z' }
+        };
+
+        link_offset = new Dictionary<int, float>()
+        {
+            {0, 101.269f},
+            {1, 0.0f},
+            {2, 89.8f},
+            {3, 86.166f},//-126.639f},
+            {4, 180.639f},
+            {5, 0},
+
+        };
+
+        link_direction = new Dictionary<int, int>()
+        {
+            {0, -1},
+            {1, 1},
+            {2, -1},
+            {3, 1},
+            {4, 1},
+            {5, 1},
+
         };
         //y, x, y, x, y, z
 
@@ -95,20 +125,44 @@ public class UnityClient : MonoBehaviour
 
     public void interact(GameObject virtual_plane_on_tcp)
     {
+        
+        if (posQueue.Count > 0)
+        {
+            cur_tcp_pos = posQueue[posQueue.Count - 1];
+            posQueue.Clear();
+            print("cur_tcp_pos is not null: " + cur_tcp_pos);
+        }
+
+        if (cur_tcp_pos == null)
+        {
+            cur_tcp_pos = new float[6] { 
+                Virtual_end_effector.transform.position.x,
+                Virtual_end_effector.transform.position.y,
+                Virtual_end_effector.transform.position.z,
+                Virtual_end_effector.transform.rotation.eulerAngles.x,
+                Virtual_end_effector.transform.rotation.eulerAngles.y,
+                Virtual_end_effector.transform.rotation.eulerAngles.z,
+               
+            };
+            
+        }
+       
+        Vector3 RobotPos = Robot2UnityCoor(new Vector3(cur_tcp_pos[0], cur_tcp_pos[1], cur_tcp_pos[2]));
+        RobotActualTCPIndicator.transform.position = RobotPos;
 
         virtual_plane_on_tcp.transform.position = VRController.transform.position + controller_endEffector_offset;
         virtual_plane_on_tcp.transform.rotation = VRController.transform.rotation;
         controller_pos = VRController.transform.position;
         Vector3 actual_movement = controller_pos - previous_controller_pos;
-        Quaternion rotation_diff = virtual_plane_on_tcp.transform.rotation * Quaternion.Inverse(previous_ee_orientation) ;
-        
+        Quaternion rotation_diff = virtual_plane_on_tcp.transform.rotation * Quaternion.Inverse(previous_ee_orientation);
+
         desired_orientation_q = rotation_diff * Virtual_end_effector.transform.rotation;
         desired_orientation_e = virtual_plane_on_tcp.transform.rotation.eulerAngles;
         desired_pos = virtual_plane_on_tcp.transform.position;
 
         Virtual_end_effector.transform.position = desired_pos;
         Virtual_end_effector.transform.rotation = desired_orientation_q;
-    
+
         //Virtual_end_effector.transform.rotation = desired_orientation_q;
 
         if (actual_movement.magnitude > 0.01) //0.01, 0.008
@@ -116,7 +170,7 @@ public class UnityClient : MonoBehaviour
             /*Vector3 actual_tcp_movement =  (VRtracker_TCP_offset + tracker_pos) - previous_tcp_pos;
             Vector3 wanted_tcp_movement = actual_tcp_movement / 10;
             desired_pos = previous_tcp_pos + wanted_tcp_movement;*/
-   
+
             Movement_indicator.transform.position = desired_pos;
             Movement_indicator.transform.rotation = desired_orientation_q;
 
@@ -130,6 +184,9 @@ public class UnityClient : MonoBehaviour
 
         }
         previous_ee_orientation = virtual_plane_on_tcp.transform.rotation;
+        /*}*/
+        
+
 
         //transform.position = desired_pos;
 
@@ -144,8 +201,9 @@ public class UnityClient : MonoBehaviour
         {
             while (trajectoryQueue.Count > 0)
             {
-                Action executeOneJointState = trajectoryQueue[0];
-                trajectoryQueue.RemoveAt(0);
+                Action executeOneJointState = trajectoryQueue[trajectoryQueue.Count - 1];
+                //trajectoryQueue.RemoveAt(0);
+                trajectoryQueue.Clear();
 
                 //Debug.Log("Trajectory Execute");
                 executeOneJointState();
@@ -163,21 +221,37 @@ public class UnityClient : MonoBehaviour
     {
         while (true)
         {
-            float[] res = Recv6Tuple();
+            float[] res;
+            string temp = Recv6Tuple(inChannel, out res);
+
+            if (temp.StartsWith("p"))
+            {
+                print("recv pos info: " + temp);
+                posQueue.Add(res);
+                print("pos result: " + res[0] + "," + res[1] + "," + res[2] + "," + res[3] + "," + res[4] + "," + res[5]);
+            }
           /*  res[2] += 263.208458;
             res[3] += 83.968;*/
             //Debug.Log("JOINT STATE RECV");
            
-
-            Action executeOneJointState = () =>
+            else
             {
-                for (int i = 0; i < res.Length; i++)
+                Parallel.For(0, 6, i => {
+                    res[i] = rad2deg(res[i]);
+                }); 
+                print("recv joint state info: " + temp);
+                print("joint result in deg: " + res[0] + "," + res[1] + "," + res[2] + "," + res[3] + "," + res[4] + "," + res[5]);
+                Action executeOneJointState = () =>
                 {
-                    move(joint_links[i], i,  res[i]);
-                }
-            };
+                    for (int i = 0; i < res.Length; i++)
+                    {
+                        move(joint_links[i], i, res[i]);
+                    }
+                };
 
-            trajectoryQueue.Add(executeOneJointState);
+                trajectoryQueue.Add(executeOneJointState);
+            }
+            
         }
 
     }
@@ -191,54 +265,66 @@ public class UnityClient : MonoBehaviour
         Debug.Log("Client close");
     }
 
-    private float[] Recv6Tuple()
+    private string Recv6Tuple(StreamReader inChannel, out float[] result)
     {
         string res = inChannel.ReadLine();
         //res = HttpUtility.UrlDecode(res, Encoding.UTF8);
         print("Res was " + res);
        // Debug.Log(res);
-        res = res.Trim(new char[] { '[', ']', 'p' });
+        string temp = res.Trim(new char[] { '[', ']', 'p' });
         //Debug.Log(res);
-        string[] each_double = res.Split(',');
-        float[] pose_result = new float[6];
+        if (temp.Contains("p"))
+        {
+            print("invalid data");
+            int charLocation = temp.IndexOf("]", StringComparison.Ordinal);
+
+            if (charLocation > 0)
+            {
+                temp = temp.Substring(0, charLocation);
+            }
+        }
+        string[] each_double = temp.Split(',');
+        result = new float[6];
         int i = 0;
         foreach (var each in each_double)
         {
-            /*print("Parsing");
-            print("each was  "  + each);*/
-            pose_result[i] = float.Parse(each);
+            /*print("Parsing");*/
+            print("each was  "  + each);
+            result[i] = float.Parse(each);
             //print("pose result was " +pose_result[i]);
-            pose_result[i] = rad2deg(pose_result[i]);// no need if return 6 tuple has p[]
+           // no need if return 6 tuple has p[]
            // print("Pose result is " + pose_result[i]);
             //Debug.Log(pose_result[i]);
             i++;
         }
-        return pose_result;
+        return res;
 
     }
 
     private void move(GameObject joint_link, int of_index, float to)
     {
         char move_which_axis;
-        if (link_index_to_axis_mapper.TryGetValue(of_index, out move_which_axis))
+        float offset;
+        int direction;
+        if (link_index_to_axis_mapper.TryGetValue(of_index, out move_which_axis) && link_offset.TryGetValue(of_index, out offset) && link_direction.TryGetValue(of_index, out direction))
         {
            // Debug.Log("rotating joint of index: " + of_index + " along: " + move_which_axis + " axis, to degree: " + to);
             if (move_which_axis == 'x')
             {
                 //Debug.Log(move_which_axis);
-                print("x Was " + joint_link.transform.localRotation.eulerAngles.x);
-                joint_link.transform.localRotation = Quaternion.Euler(to, joint_link.transform.localRotation.eulerAngles.y, joint_link.transform.localRotation.eulerAngles.z);
-                print("x is " + joint_link.transform.localRotation.eulerAngles.x);
+                //print("x Was " + joint_link.transform.localRotation.eulerAngles.x);
+                joint_link.transform.localRotation = Quaternion.Euler(to * direction + offset, joint_link.transform.localRotation.eulerAngles.y, joint_link.transform.localRotation.eulerAngles.z);
+                //print("x is " + joint_link.transform.localRotation.eulerAngles.x);
             }
             else if (move_which_axis == 'y')
             {
                 //Debug.Log(move_which_axis);
-                joint_link.transform.localRotation = Quaternion.Euler(joint_link.transform.localRotation.eulerAngles.x, to, joint_link.transform.localRotation.eulerAngles.z);
+                joint_link.transform.localRotation = Quaternion.Euler(joint_link.transform.localRotation.eulerAngles.x, to * direction + offset, joint_link.transform.localRotation.eulerAngles.z);
             }
             else if (move_which_axis == 'z')
             {
                 //Debug.Log(move_which_axis);
-                joint_link.transform.localRotation = Quaternion.Euler(joint_link.transform.localRotation.eulerAngles.x, joint_link.transform.localRotation.eulerAngles.y, to);
+                joint_link.transform.localRotation = Quaternion.Euler(joint_link.transform.localRotation.eulerAngles.x, joint_link.transform.localRotation.eulerAngles.y, to * direction + offset);
             }
         }
         else
@@ -253,14 +339,21 @@ public class UnityClient : MonoBehaviour
         Vector3 FLU = Unity2RobotCoor(desired_pos);
 
         Vector3 axisAngle = Quaternion2axisAngle(desired_orientation);
+        /*float angle;
+        Vector3 axis;
+
+        desired_orientation.ToAngleAxis(out angle, out axis);
+        axisAngle = new Vector3(axis.z, -axis.x, axis.y) * angle;*/
 
 
         string pose_6_tuple = "(" + FLU.x + "," + FLU.y + "," + FLU.z + ","
             + axisAngle.x + "," + axisAngle.y + "," + axisAngle.z + ")";
+        pose_6_tuple = "(" + FLU.x + "," + FLU.y + "," + FLU.z + ","
+            + 0.0 + "," + 0.0 + "," + 0.0 + ")";
         return pose_6_tuple;
     }
 
-    private string packCommand(Vector3 desired_pos, Vector3 desired_orientation)
+   /* private string packCommand(Vector3 desired_pos, Vector3 desired_orientation)
     {
 
         double x = desired_pos.z;
@@ -272,9 +365,9 @@ public class UnityClient : MonoBehaviour
             , desired_orientation.z);
 
         string pose_6_tuple = "(" + x + "," + y + "," + z + ","
-            + axisAngle.x + "," + axisAngle.y + "," + axisAngle.z + ")\n";
+            + axisAngle.x + "," + axisAngle.y + "," + axisAngle.z + ")"; //]n
         return pose_6_tuple;
-    }
+    }*/
     private float rad2deg(double rad)
     {
     /*    Debug.Log("was: " + rad);
